5c98efd7a9d993d58dd9a78c60e5a72a828de239
KimNganTP5813 <akinosora13@gmail.com>
2025-04-17 01:51:33 +0900

    Update btvn1-3.js

--- btvn1-3.js
+++ btvn1-3.js
@@ -6,8 +6,8 @@ function createUser(name, age = 18, role = "user") {
   };
 }
 
-console.log(createUser("Alice"));
-// Output với giá trị mặt định: { name: 'Alice', age: 18, role: 'user' }
+console.log(createUser("Nguyễn B"));
+// Output với giá trị mặt định: { name: 'Nguyễn B', age: 18, role: 'user' }
 
-console.log(createUser("Bob", 25, "admin"));
+console.log(createUser("Nguyễn Văn A", 20, "admin"));
 // Output: { name: 'Bob', age: 25, role: 'admin' }
